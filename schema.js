fd7e3ebc9a1f93d412194f1a52bc6ba99883e9c3
bhoomi-sahu <sbhoomi@gmail.com>
2025-07-20 08:30:04 +0530

    Added reviewSchema to fix Joi validation error

--- schema.js
+++ schema.js
@@ -1,5 +1,6 @@
 const joi = require("joi");
 
+
 module.exports.listingSchema = joi.object({
   listing: joi.object({
     title: joi.string().required(),
@@ -8,9 +9,14 @@ module.exports.listingSchema = joi.object({
     country: joi.string().required(),
     price: joi.string().required().min(0),
     image: joi.string().allow("", null),
-
-    // ✅ ADD CATEGORY FIELD HERE
     category: joi.string().valid("Hotel", "PG", "Room", "Flat", "Colony").required()
+  }).required(),
+});
 
+
+module.exports.reviewSchema = joi.object({
+  review: joi.object({
+    rating: joi.number().required().min(1).max(5),
+    comment: joi.string().required()
   }).required(),
 });
